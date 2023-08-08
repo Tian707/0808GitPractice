7e4ad892954922855bfd419030df101e3abc5a60
Tian707 <ieason1028@hotmail.com>
2023-08-08 12:12:43 +0200

    First commit

---
+++ 0808GitPractice/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0808GitPractice
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Telephone { get; set; }
+
+        public Person(string name, int telephone)
+        {
+            Name = name;
+            Telephone = telephone;
+            Console.WriteLine($"Name: {name}, Tel: {telephone}");
+        }
+
+    }
+}
---
+++ 0808GitPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _0808GitPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Lily", 1235467);
+        }
+    }
+}
